ced73535ca9121b32d95b2eca31fcada11d7d917
Nick Conklin <nconkl1@outlook.com>
2018-03-22 16:59:13 -0400

    Deleted models.py, Moved from SQLite to PostGreSQL and modified code accordingly

--- geoip.py
+++ geoip.py
@@ -1,9 +1,6 @@
-import sqlite3
-from sqlite3 import Error
 import geoip2.database
 import configparser
 from flask import request
-from flask import jsonify
 
 configuration = configparser.ConfigParser()  # Initialize configparser
 configuration.read('config/config.ini')
@@ -14,8 +11,10 @@ country_database_location = configuration['MAXMIND']['CountryDatabaseLocation']
 reader_city = geoip2.database.Reader(city_database_location)
 reader_country = geoip2.database.Reader(country_database_location)
 
+
 def get_ip_address():
-    return jsonify({'ip': request.remote_addr}), 200
+    return request.remote_addr
+
 
 def get_city_lon_lat(ip):
     response = reader_city.city(ip)
@@ -24,8 +23,13 @@ def get_city_lon_lat(ip):
     latitude = response.location.latitude
     longitude = response.location.longitude
 
-    return jsonify({'city': city_name, 'latitude': latitude, 'longitude': longitude}), 200
+    return city_name, latitude, longitude
+
 
 def get_country(ip):
     response = reader_country.country(ip)
-    return jsonify({'country': response}), 200
+
+    country_name = response.country.name
+
+    return country_name
+
--- main.py
+++ main.py
@@ -1,26 +1,80 @@
-from flask import Flask, render_template
-import models
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 import geoip
 import configparser
 
 app = Flask(__name__)
 
-configuration = configparser.ConfigParser()  # Initialize configparser
-configuration.read('config/config.ini')
+config = configparser.ConfigParser()  # Initialize configparser
+config.read('config/config.ini')
 
-sqlite_location = configuration['SQLITE']['Location']
+# Config Data for the PostGre URI
+user = config['POSTGRE']['User']
+password = config['POSTGRE']['Password']
+host = config['POSTGRE']['Host']
+port = config['POSTGRE']['Port']
+database = config['POSTGRE']['Database']
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:\\Users\\nconk\\PyCharmProjects\\IPGeoLocator\\test.db'
+    #f'postgresql://{user}:{password}@{host}:{port}/{database}'
+db = SQLAlchemy(app)
+
+
+# Table class to create table when db.create_all() is called
+class LocationData(db.Model):
+    ip = db.Column(db.String, primary_key=True)
+
+    def __repr__(self):
+        return '<IP Address: {}'.format(self.ip)
+
+    def get_data(self):
+        ip_address = '216.246.161.55'
+        city = geoip.get_city_lon_lat(ip_address)[0]
+        country = geoip.get_country(ip_address)
+        latitude = geoip.get_city_lon_lat(ip_address)[1]
+        longitude = geoip.get_city_lon_lat(ip_address)[2]
+
+        return {'ip': ip_address, 'city': city, 'country': country, 'latitude': latitude, 'longitude': longitude}
+
+    @property
+    def city(self):
+        return self.get_data()['city']
+    
+    @property
+    def country(self):
+        return self.get_data()['country']
+    
+    @property
+    def latitude(self):
+        return self.get_data()['latitude']
+
+    @property
+    def longitude(self):
+        return self.get_data()['longitude']
+
+# Create the database and tables
+db.create_all()
 
 @app.route('/')
 def welcome():
-    return render_template('welcome.html')
+    return render_template('welcome.html'), 200
 
 
-@app.route('/index', methods = ['POST'])
+@app.route('/about')
+def about():
+    return render_template('about.html'), 200
+
+
+@app.route('/index', methods = ['GET', 'POST'])
 def index():
-    models.create_connection(sqlite_location)
-    return render_template('index.html')
+    user = LocationData(ip='216.246.161.55')
+    data = LocationData.query.all()
+    print(data)
+    print(user.city)
+    db.session.add(user)
+    db.session.commit()
+    return render_template('index.html', data = [user.country, user.city]), 200
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug = True)
